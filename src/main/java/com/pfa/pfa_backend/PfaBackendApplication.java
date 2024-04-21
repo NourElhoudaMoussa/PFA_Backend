2c670f8c0a12b0a0f7df7bb45387be3e50611522
NourElhoudaMoussa <moussanouralhouda@gmail.com>
2024-04-21 15:15:59 +0100

    Initial commit

--- src/main/java/com/pfa/pfa_backend/PfaBackendApplication.java
+++ src/main/java/com/pfa/pfa_backend/PfaBackendApplication.java
@@ -8,6 +8,7 @@ public class PfaBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PfaBackendApplication.class, args);
+        System.out.println("run successfully");
     }
 
 }
